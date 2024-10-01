aba6a2f7974e71f6b7d44d2b29de717a6ac39bea
Brendan Kettle <38698119+brendankettle@users.noreply.github.com>
2024-10-01 11:10:57 -0400

    Update Interferometer.py

--- diagnostics/Interferometer.py
+++ diagnostics/Interferometer.py
@@ -21,4 +21,22 @@ class Interferometer(Diagnostic):
         """Initiate parent base Diagnostic class to get all shared attributes and funcs"""
         super().__init__(exp_obj, config_filepath)
         return
+
+    def get_proc_shot(self, shot_dict, calib_id=None, roi=None, debug=False):
+        """Return a processed shot using saved or passed calibrations.
+        """
+        # set calibration dictionary
+        if calib_id:
+            self.calib_dict = self.get_calib(calib_id)
+        else:
+            self.calib_dict = self.get_calib(shot_dict)
+        
+        # do standard image calibration. Transforms, background, ROIs etc.
+        img, x, y = self.run_img_calib(shot_dict, debug=debug)
+
+        self.curr_img = img
+        self.x = x
+        self.y = y
+
+        return img, x, y
     
